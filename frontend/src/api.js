dce85c1754a337f096d535efddce23e801357e4d
shreyash Satone <145322889+Shreyashsatone@users.noreply.github.com>
2025-02-09 23:47:06 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://event-czm8.onrender.com/api',
   withCredentials: true,  // Ensures cookies are sent
   headers: {
     'Content-Type': 'application/json'
